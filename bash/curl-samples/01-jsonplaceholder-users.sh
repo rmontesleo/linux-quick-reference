3bf7926086169382de2712dccd40775e74510c4b
Leoopoldo Ramirez Montes <rmontesleo@gmail.com>
2022-06-24 01:13:25 -0500

    add samples with curl

---
+++ bash/curl-samples/01-jsonplaceholder-users.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+
+### Gete all users
+curl -X GET https://jsonplaceholder.typicode.com/users
+
+### Save the response in a variable
+all_users=$(curl -X GET https://jsonplaceholder.typicode.com/users)
+
+### print all users
+echo  ""
+echo "All users $all_users"
+
+### first user
+echo  ""
+first_user=$( echo $all_users | jq '.[0]' )
+echo "First user is ${first_user}"
+
+
+### print email of first record
+echo  ""
+email=$( echo $first_user | jq '.email' )
+echo "email is $email"
+
+### print row email of first record
+echo ""
+row_email=$( echo $first_user | jq -r '.email' )
+echo "row email is $row_email"
+
+
+### last user
+echo ""
+last_user=$( echo $all_users | jq '.[-1]' )
+echo "Last user is ${last_user}"
+
+
+### the number of users
+echo ""
+users_length=$( echo $all_users | jq ' . | length' )
+echo "Number of items is $users_length"
+
+
+### Get status code
+echo ""
+http_code=$(curl --write-out "%{http_code}\n" --output /dev/null --silent  https://jsonplaceholder.typicode.com/users)
+echo $http_code 
